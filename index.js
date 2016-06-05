ff32d80479fea80002242c62a2bf8e83ca9d8275
TEMU3000 <b03902051@csie.ntu.edu.tw>
2016-06-05 21:51:33 +0800

    can move by mobile

---
+++ index.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+/*global.document = document;
+global.Phaser = Phaser = require('Phaser/build/custom/phaser-arcade-physics');*/
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use("/assets", express.static(__dirname + '/assets'));
+
+/*app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+  console.log('get');
+});*/
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  io.emit('connectOK', 'OK');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('message', function(msg){
+    console.log('message: ' + msg);
+  });
+  socket.on('X', function(msg){
+    console.log('X: ' + msg);
+	io.emit('setX', msg);
+  });
+  socket.on('Y', function(msg){
+    console.log('Y: ' + msg);
+	io.emit('setY', msg);
+  });
+  socket.on('Z', function(msg){
+    console.log('Z: ' + msg);
+	io.emit('connectOK', 'OK');
+  });
+  /*socket.on('zShift', function(msg){
+    console.log('zShift: ' + msg);
+    io.emit('change_zShift', {
+        zShift: msg
+    });
+  });*/
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
