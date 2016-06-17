b878e14d6945504d8ea9722964587b79c25539ea
TEMU3000 <b03902051@csie.ntu.edu.tw>
2016-06-17 20:26:51 +0800

    server can serve single and multi at same code

--- index.js
+++ index.js
@@ -28,54 +28,111 @@ io.on('connection', function(socket){
   io.emit('connectOK', 'OK');
   socket.on('disconnect', function(){
     console.log('user disconnected');
+	player1status = false;
+	player2status = false;
+	io.emit(player1uuid, 'checkConnect');
+	io.emit(player2uuid, 'checkConnect');
   });
+  //=====Android to Web
   socket.on('message', function(msg){
-    //console.log('message: ' + msg);
 	if(msg == 'fire'){
 		io.emit('shoot', msg);
 	}else if(msg == 'start'){
-    console.log('start game');
 		io.emit('start', msg);
 	}
   });
+  socket.on('message1', function(msg){
+	if(msg == 'fire'){
+		io.emit('shoot1', msg);
+	}else if(msg == 'start'){
+		io.emit('start1', msg);
+	}
+  });
+  socket.on('message2', function(msg){
+	if(msg == 'fire'){
+		io.emit('shoot2', msg);
+	}else if(msg == 'start'){
+		io.emit('start2', msg);
+	}
+  });
   socket.on('X', function(msg){
-    //console.log('X: ' + msg);
 	   io.emit('setX', msg);
   });
   socket.on('Y', function(msg){
-    //console.log('Y: ' + msg);
 	   io.emit('setY', msg);
   });
-  socket.on('vibrate', function(msg){
-	   io.emit('connectOK', msg);
+  socket.on('X1', function(msg){
+	   io.emit('setX1', msg);
   });
-  socket.on('ULT', function(msg){
-    //console.log('Y: ' + msg);
-	   io.emit('connectOK', msg);
+  socket.on('Y1', function(msg){
+	   io.emit('setY1', msg);
+  });
+  socket.on('X2', function(msg){
+	   io.emit('setX2', msg);
+  });
+  socket.on('Y2', function(msg){
+	   io.emit('setY2', msg);
   });
   socket.on('ultra', function(msg){
-	   if(msg == 'start')  {
-       console.log('start game');
-       io.emit('start', msg);
-     }
+	   if(msg == 'start') io.emit('start', msg);
 	    else io.emit('ultra', msg);
   });
   socket.on('switch_weapon', function(msg){
 	   io.emit('switch_weapon', msg);
   });
-  socket.on('data', function(data)  {
-    SaveToDB(data);
+  //=====
+  
+  //=====Web to Android
+  socket.on('vibrate', function(msg){
+	   io.emit('connectOK', msg);
   });
-  /*socket.on('Z', function(msg){
-    //console.log('Z: ' + msg);
-	io.emit('connectOK', 'OK');
-  });*/
-  /*socket.on('zShift', function(msg){
-    console.log('zShift: ' + msg);
-    io.emit('change_zShift', {
-        zShift: msg
-    });
-  });*/
+  socket.on('ULT', function(msg){
+	   io.emit('connectOK', msg);
+  });
+  socket.on('vibrate1', function(msg){
+	   io.emit(player1uuid, msg);
+  });
+  socket.on('ULT1', function(msg){
+	   io.emit(player1uuid, msg);
+  });
+  socket.on('vibrate2', function(msg){
+	   io.emit(player2uuid, msg);
+  });
+  socket.on('ULT2', function(msg){
+	   io.emit(player2uuid, msg);
+  });
+  //=====
+  
+  //=====Android to server  
+  socket.on('requestPlayer', function(msg){
+	if(msg == player1uuid || msg == player2uuid){return;}
+	if(!player1status){
+		player1status = true;
+		player1uuid = msg;
+		io.emit(player1uuid, 'player1');
+		//server to web
+		io.emit('players', '2');
+	}else if(!player2status){
+		player2status = true;
+		player2uuid = msg;
+		io.emit(player2uuid, 'player2');
+		io.emit(player1uuid, 'ready');
+		io.emit(player2uuid, 'ready');
+		//server to web
+		io.emit('players', 'go');
+	}else{
+		io.emit(msg, 'full');
+	}
+  });
+  socket.on('stillConnect', function(msg){
+	if(msg == player1uuid){
+		player1status = true;
+	}
+	if(msg == player2uuid){
+		player2status = true;
+	}
+  });
+  //=====
 });
 
 http.listen(3000, function(){
