2ecbc155bf006b0ad27cccc0ac193c55bc61d455
treetree <b03902043@ntu.edu.tw>
2016-06-19 20:40:45 +0800

    heroku listen port

--- index.js
+++ index.js
@@ -138,8 +138,8 @@ io.on('connection', function(socket){
   //=====
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on *: %d', process.env.PORT);
 });
 
 var scoreBoard;
