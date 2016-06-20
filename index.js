e99336f60caa84be0b5826b26d3a047a5a58aec5
treetree <b03902043@ntu.edu.tw>
2016-06-20 10:19:08 +0800

    add magic number at each web

--- index.js
+++ index.js
@@ -16,6 +16,23 @@ var player1uuid;
 var player2status = false;
 var player2uuid;
 
+var Magics = new Array();
+
+var crypto = require('crypto');
+function random (howMany, chars) {
+    chars = chars
+        || "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+    var rnd = crypto.randomBytes(howMany)
+        , value = new Array(howMany)
+        , len = chars.length;
+
+    for (var i = 0; i < howMany; i++) {
+        value[i] = chars[rnd[i] % len]
+    };
+
+    return value.join('');
+}
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -25,118 +42,154 @@ app.use("/assets", express.static(__dirname + '/assets'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  io.emit('connectOK', 'OK');
+
+  // first time shake hand, magic number on web
+  // mobile <=> server
+  socket.on('magic', function(magic) {
+    console.log('magic = %s', String(magic));
+    // check magic number is exist or not
+    if( Magics.indexOf(String(magic)) > -1 )  {
+      io.emit('connectOK', 'OK');
+      socket.mobileMagic = String(magic);
+      addMobileOwnSocket(socket, String(magic));
+    }else {
+      io.emit('connectOK', 'Failed');
+    }
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
-	player1status = false;
-	player2status = false;
-	io.emit(player1uuid, 'checkConnect');
-	io.emit(player2uuid, 'checkConnect');
+  	player1status = false;
+  	player2status = false;
+  	io.emit(player1uuid, 'checkConnect');
+  	io.emit(player2uuid, 'checkConnect');
+    if( socket.magic != null )  {
+      var id = Magics.indexOf(socket.magic);
+      Magics.splice(id, 1);
+    }
   });
-  //=====Android to Web
-  socket.on('message', function(msg){
-	if(msg == 'fire'){
-		io.emit('shoot', msg);
-	}else if(msg == 'start'){
-		io.emit('start', msg);
-	}
-  });
-  socket.on('message1', function(msg){
-	if(msg == 'fire'){
-		io.emit('shoot1', msg);
-	}else if(msg == 'start'){
-		io.emit('start1', msg);
-	}
-  });
-  socket.on('message2', function(msg){
-	if(msg == 'fire'){
-		io.emit('shoot2', msg);
-	}else if(msg == 'start'){
-		io.emit('start2', msg);
-	}
-  });
-  socket.on('X', function(msg){
-	   io.emit('setX', msg);
-  });
-  socket.on('Y', function(msg){
-	   io.emit('setY', msg);
-  });
-  socket.on('X1', function(msg){
-	   io.emit('setX1', msg);
-  });
-  socket.on('Y1', function(msg){
-	   io.emit('setY1', msg);
-  });
-  socket.on('X2', function(msg){
-	   io.emit('setX2', msg);
-  });
-  socket.on('Y2', function(msg){
-	   io.emit('setY2', msg);
-  });
-  socket.on('ultra', function(msg){
-	   if(msg == 'start') io.emit('start', msg);
-	    else io.emit('ultra', msg);
-  });
-  socket.on('switch_weapon', function(msg){
-	   io.emit('switch_weapon', msg);
-  });
-  socket.on('data', function(data){
-	   SaveToDB(data);
-  });
-  //=====
 
-  //=====Web to Android
-  socket.on('vibrate', function(msg){
-	   io.emit('connectOK', msg);
+  // Web to server
+  socket.on('reqMagic', function()  {
+    var magic = random(5);
+    // TODO: check duplication of random string
+    console.log('random magic = %s', String(magic));
+    io.emit('getMagic', magic);
+    Magics.push(String(magic));
+    socket.magic = String(magic);
+    for( var i = 0 ; i < Magics.length ; i ++ )
+        console.log('magic[%d] = %s', i, Magics[i]);
+    addWebOwnSocket(socket, String(magic));
   });
-  socket.on('ULT', function(msg){
-	   io.emit('connectOK', msg);
-  });
-  socket.on('vibrate1', function(msg){
-	   io.emit(player1uuid, msg);
-  });
-  socket.on('ULT1', function(msg){
-	   io.emit(player1uuid, msg);
-  });
-  socket.on('vibrate2', function(msg){
-	   io.emit(player2uuid, msg);
-  });
-  socket.on('ULT2', function(msg){
-	   io.emit(player2uuid, msg);
-  });
-  //=====
 
   //=====Android to server
   socket.on('requestPlayer', function(msg){
-	if(msg == player1uuid || msg == player2uuid){return;}
-	if(!player1status){
-		player1status = true;
-		player1uuid = msg;
-		io.emit(player1uuid, 'player1');
-		//server to web
-		io.emit('players', '2');
-	}else if(!player2status){
-		player2status = true;
-		player2uuid = msg;
-		io.emit(player2uuid, 'player2');
-		io.emit(player1uuid, 'ready');
-		io.emit(player2uuid, 'ready');
-		//server to web
-		io.emit('players', 'go');
-	}else{
-		io.emit(msg, 'full');
-	}
+  	if(msg == player1uuid || msg == player2uuid){return;}
+  	if(!player1status){
+  		player1status = true;
+  		player1uuid = msg;
+  		io.emit(player1uuid, 'player1');
+  		//server to web
+  		io.emit('players', '2');
+  	}else if(!player2status){
+  		player2status = true;
+  		player2uuid = msg;
+  		io.emit(player2uuid, 'player2');
+  		io.emit(player1uuid, 'ready');
+  		io.emit(player2uuid, 'ready');
+  		//server to web
+  		io.emit('players', 'go');
+  	}else{
+  		io.emit(msg, 'full');
+  	}
   });
   socket.on('stillConnect', function(msg){
-	if(msg == player1uuid){
-		player1status = true;
-	}
-	if(msg == player2uuid){
-		player2status = true;
-	}
+  	if(msg == player1uuid){
+  		player1status = true;
+  	}
+  	if(msg == player2uuid){
+  		player2status = true;
+  	}
   });
   //=====
 });
+
+function addWebOwnSocket(socket, magic)   {
+  //=====Web to Android
+  socket.on('vibrate' + magic, function(msg){
+     io.emit('connectOK'+magic, msg);
+  });
+  socket.on('ULT' + magic, function(msg){
+     io.emit('connectOK'+magic, msg);
+  });
+  socket.on('vibrate1' + magic, function(msg){
+     io.emit(player1uuid, msg);
+  });
+  socket.on('ULT1' + magic, function(msg){
+     io.emit(player1uuid, msg);
+  });
+  socket.on('vibrate2' + magic, function(msg){
+     io.emit(player2uuid, msg);
+  });
+  socket.on('ULT2' + magic, function(msg){
+     io.emit(player2uuid, msg);
+  });
+  socket.on('data' + magic, function(data){
+     SaveToDB(data);
+  });
+  //=====
+}
+
+function addMobileOwnSocket(socket, magic) {
+  //=====Android to Web
+  socket.on('message' + magic, function(msg){
+    if(msg == 'fire'){
+      io.emit('shoot' + magic, msg);
+    }else if(msg == 'start'){
+      io.emit('start' + magic, msg);
+    }
+  });
+  socket.on('message1' + magic, function(msg){
+    if(msg == 'fire'){
+      io.emit('shoot1' + magic, msg);
+    }else if(msg == 'start'){
+      io.emit('start1' + magic, msg);
+    }
+  });
+  socket.on('message2' + magic, function(msg){
+    if(msg == 'fire'){
+      io.emit('shoot2' + magic, msg);
+    }else if(msg == 'start'){
+      io.emit('start2' + magic, msg);
+    }
+  });
+  socket.on('X' + magic, function(msg){
+     io.emit('setX'+magic, msg);
+  });
+  socket.on('Y' + magic, function(msg){
+     io.emit('setY'+magic, msg);
+  });
+  socket.on('X1' + magic, function(msg){
+     io.emit('setX1' + magic, msg);
+  });
+  socket.on('Y1' + magic, function(msg){
+     io.emit('setY1' + magic, msg);
+  });
+  socket.on('X2' + magic, function(msg){
+     io.emit('setX2' + magic, msg);
+  });
+  socket.on('Y2' + magic, function(msg){
+     io.emit('setY2' + magic, msg);
+  });
+  socket.on('ultra' + magic, function(msg){
+     if(msg == 'start') io.emit('start'+magic, msg);
+      else io.emit('ultra'+magic, msg);
+  });
+  socket.on('switch_weapon' + magic, function(msg){
+     io.emit('switch_weapon'+magic, msg);
+  });
+  //=====
+}
 
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *: %d', process.env.PORT);
